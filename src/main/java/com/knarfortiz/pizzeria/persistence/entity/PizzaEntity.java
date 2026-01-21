b0eef4798a862913bf0d72a79d52f84957fb9f75
knarfortiz <o.g.knarf@hotmail.com>
2026-01-20 21:37:57 -0500

    add entity classes for Order and Pizza with JPA annotations

---
+++ src/main/java/com/knarfortiz/pizzeria/persistence/entity/PizzaEntity.java
@@ -0,0 +1,33 @@
+package com.knarfortiz.pizzeria.persistence.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "pizza")
+@Getter @Setter
+@NoArgsConstructor
+public class PizzaEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_pizza")
+    private Integer id_pizza;
+
+    @Column(nullable = false, length = 30, unique = true)
+    private String name;
+
+    @Column(nullable = false, length = 150)
+    private String description;
+
+    @Column(nullable = false, columnDefinition = "DECIMAL(5,2)")
+    private Double price;
+
+    private Boolean vegetarian;
+
+    private Boolean vegan;
+
+    @Column(nullable = false)
+    private Boolean available;
+}
